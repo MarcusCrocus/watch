c83f97611db8fedda7cd0889396af706b31f181a
Marcus <crocus.html@gmail.com>
2020-10-03 19:57:15 +0200

    fixed buggs on syncronisation with style.min

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync');
+const sass         = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const rename       = require("gulp-rename");
 
 gulp.task('server', function() {
 
-    browserSync({
+    browserSync.init({
         server: {
             baseDir: "src"
         }
@@ -16,7 +16,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
@@ -26,7 +26,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', browserSync.reload);
 })
 
