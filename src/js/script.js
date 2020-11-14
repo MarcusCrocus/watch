6a0d44e268c69e90ca87ab4bd3ee6bc44d72a8e8
Marcus <crocus.html@gmail.com>
2020-11-14 19:49:28 +0100

    content cards block were finished

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,41 @@ $(document).ready(function(){
             }
         ]
     });
+                                    //https://denis-creative.com/jquery-tabs/
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+   /*  $('.catalog-item__link').each(function(i){
+        $(this).on('click', function(e) {
+            e.preventDefault(); //предотвратить стандартное поведение браузера
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active'); //что будет при клике
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+
+    $('.catalog-item__back').each(function(i){
+        $(this).on('click', function(e) {
+            e.preventDefault(); //предотвратить стандартное поведение браузера
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active'); //что будет при клике
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    }); */
+                                    // оптимизация ниже
+    function toggleSlide(item) {
+        $(item).each(function(i){
+            $(this).on('click', function(e) {
+                e.preventDefault(); //предотвратить стандартное поведение браузера
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active'); //что будет при клике
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
 
 /*                                             //Js carousel
@@ -98,3 +133,4 @@ let slider = tns({
     slideBy: 'page',
     autoplay: true
 }); */
+
